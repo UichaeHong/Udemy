e5ea76af9cb4cff92bbe8e9574c0efa5ab0b1443
Uichae <ghddmlco@gmail.com>
2023-07-19 16:42:39 +0900

    mynext_js

---
+++ mynext_js/app/about/page.js
@@ -0,0 +1,9 @@
+function About() {
+  return (
+    <div>
+      <h1>여기는 About페이지</h1>
+    </div>
+  );
+}
+
+export default About;
--- mynext_js/app/page.js
+++ mynext_js/app/page.js
@@ -1,9 +1,11 @@
 // import Image from "next/image";
+// import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <div></div>
+      <div>create-next-app</div>
+      <div>안녕 여기는 홈</div>
     </div>
   );
 }
